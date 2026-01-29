1449cfc85c83f4b5014df2ab90e67470bb9d965c
ardaaslankirdar <aslan.kirdar@ug.bilkent.edu.tr>
2026-01-29 10:25:25 +0300

    Fixed the sum methods

--- ArrayApp.java
+++ ArrayApp.java
@@ -5,7 +5,7 @@ public class ArrayApp {
     
     /**
      * Finds the maximum value in an integer array.
-     * @param arr
+     * @param arr the input array
      * @return maximum value in the array
      */
     public int findMax(int[] arr) {
@@ -20,7 +20,7 @@ public class ArrayApp {
 
     /**
      * Finds the minimum value in an integer array.
-     * @param arr
+     * @param arr the input array
      * @return minimum value in the array
      */
     public int findMin(int[] arr) {
@@ -38,13 +38,29 @@ public class ArrayApp {
         return null;
     }
 
+    /**
+     * Finds the sum of odd-numbered indexes in the array
+     * @param arr the input array
+     * @return sum of odd-numbered indexes in the array
+     */
     public int findSumOfOddNumberedIndexes(int[] arr) {
-        // TO DO: Implement this method
-        return 0;
+        int sum = 0;
+        for (int i = 1; i < arr.length; i += 2) {
+            sum += arr[i];
+        }
+        return sum;
     }
 
+    /**
+     * Finds the sum of odd-numbered indexes in the array
+     * @param arr the input array
+     * @return sum of odd-numbered indexes in the array
+     */
     public int findSumOfEvenNumberedIndexes(int[] arr) {
-        // TO DO: Implement this method
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i += 2) {
+            sum += arr[i];
+        }
+        return sum;
     }
 }
